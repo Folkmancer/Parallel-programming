490c948d35af5a029d54552a4eb1b05ed6306d1e
Artyom Dolgushin <folkmancer@gmail.com>
2018-06-01 19:28:39 +0500

    MPI_Bcast final

--- Lab7.1/Source.cpp
+++ Lab7.1/Source.cpp
@@ -11,7 +11,7 @@
 int main(int argc, char **argv)
 {
 	int size, rank, sendcount = N, recvcount = N, source = 1, root = 0;
-	float recvbufScatter[N], *recvbufGather;
+	float recvbufScatter[N], *recvbufGather, *sendbufBcast;
 
 	float sendbuf[N][N] = {
 		{ 1.0, 2.0, 3.0, 4.0 },
@@ -25,7 +25,7 @@ int main(int argc, char **argv)
 	MPI_Comm_size(MPI_COMM_WORLD, &size);
 
 	if (size == N) {
-		/*MPI_Scatter(sendbuf, sendcount, MPI_FLOAT, recvbufScatter, recvcount,
+		MPI_Scatter(sendbuf, sendcount, MPI_FLOAT, recvbufScatter, recvcount,
 			MPI_FLOAT, source, MPI_COMM_WORLD);
 
 		printf("rank= %d  Results: %f %f %f %f\n", 
@@ -34,7 +34,7 @@ int main(int argc, char **argv)
 			recvbufScatter[1],
 			recvbufScatter[2], 
 			recvbufScatter[3]
-		);*/
+		);
 
 		recvbufGather = (float *)malloc(size * N * sizeof(float));
 		MPI_Gather(sendbuf[rank], N, MPI_FLOAT, recvbufGather, recvcount, MPI_FLOAT, root, MPI_COMM_WORLD);
@@ -43,6 +43,14 @@ int main(int argc, char **argv)
 				printf("rank= %d line = %d Results: %f\n", rank, i, recvbufGather[i]);
 			}
 		}
+		
+		sendbufBcast = new float[6];
+		for (int i = 0; i < 6; i++) {
+			sendbufBcast[i] = recvbufGather[i];
+		}
+		MPI_Bcast(sendbufBcast, 6, MPI_FLOAT, root, MPI_COMM_WORLD);
+		printf("I am %d process. I received elements: %f, %f, %f, %f, %f, %f", rank, sendbufBcast[0],
+			sendbufBcast[1], sendbufBcast[2], sendbufBcast[3], sendbufBcast[4], sendbufBcast[5]);
 	}
 	else
 		printf("Must specify %d processors. Terminating.\n", N);
